0f75701a3a975614f9cd41e6ba730f5ab911329b
Steven Tebbs <steventebbs@Stevens-MacBook-Pro.local>
2018-03-02 16:08:26 -0700

    Completed friendly routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301223322) do
+ActiveRecord::Schema.define(version: 20180302222801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20180301223322) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
