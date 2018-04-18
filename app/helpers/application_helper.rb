a0671fbcd4e418b015f678a4d4c4ad24228b29f7
Steven Tebbs <sven_was_here@yahoo.com>
2018-04-18 11:27:28 -0600

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,10 @@ module ApplicationHelper
 		if current_user.is_a?(GuestUser)
 			(link_to "Register", new_user_registration_path, class: style) + 
         	" ".html_safe + 
-        	(link_to "Login", new_user_session_path, class: style)
-    	else 
-        	link_to "logout", destroy_user_session_path, method: :delete, class: style
-    	end  
+      (link_to "Login", new_user_session_path, class: style)
+    else 
+      link_to "logout", destroy_user_session_path, method: :delete, class: style
+    end  
 	end
 
 	def source_helper(layout_name)
@@ -14,5 +14,44 @@ module ApplicationHelper
 			greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
 			content_tag(:p, greeting, class: "source-greeting")
 		end	
+  end
+
+  def nav_items
+   [
+     {
+       url: root_path,
+       title: 'Home'
+     },
+     {
+       url: about_me_path,
+       title: 'About Me'
+     },
+     {
+       url: contact_path,
+       title: 'Contact'
+     },
+     {
+       url: blogs_path,
+       title: 'Blog'
+     },
+     {
+       url: portfolios_path,
+       title: 'Portfolio'
+     },
+   ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
 end
