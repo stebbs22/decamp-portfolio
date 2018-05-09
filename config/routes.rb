501c8ce68cdd3fa04e3e8b08e5e78757c431243b
Steven Tebbs <sven_was_here@yahoo.com>
2018-05-09 11:20:42 -0600

    Created a comment broadcast, configured the websocket route, and updated the comment model with a callback. Also the functionality of twitter was deleted and so reimplemented live twitter feed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
   end
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-
  
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'nba-news', to: 'pages#nba_news'
+
 
   resources :blogs do
     member do
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   end
 end
 
+  mount ActionCable.server => '/cable'
 
   root to: 'pages#home'
 end
